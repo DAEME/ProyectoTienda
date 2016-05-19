a9ab34083374a7ef19e795dedcc96983bd68fe59
Eric Rodrich <acron.bn@gmail.com>
2016-05-19 12:46:14 -0500

    Correcciones varias - Web Services funcionando

--- WCFTiendaServices/Persistencia/ConexionUtil.cs
+++ WCFTiendaServices/Persistencia/ConexionUtil.cs
@@ -9,7 +9,7 @@ namespace WCFTiendaServices.Persistencia
     {
         public static string ObtenerCadena()
         {
-            // return "Data Source=(local)\\SQL2012;Initial Catalog=BDTienda;Integrated Security=SSPI;";
+            //return "Data Source=INCAD_PC001\\SQLEXPRESS;Initial Catalog=BDProyectoTienda;Integrated Security=SSPI;User Id=Eric;";
             return "Data Source=4d0d91c6-2d3d-41a3-b56f-a60b00947775.sqlserver.sequelizer.com;Initial Catalog=db4d0d91c62d3d41a3b56fa60b00947775;User ID=fwoetykjuanjzoyi;Password=3RVHSF2kE5eYi2MxCE4HPLvAmZoKpSCS2BY5zvQNaxMY3Gn5vh2xMxMpuuvZsq5U;";
         }
     }
