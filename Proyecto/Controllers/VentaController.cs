b8f65f183bdd7b0c77e1515f86bd932dfb61f698
Sebastián Tello <sebas.pujat16@gmail.com>
2016-05-21 08:11:38 -0500

    Proy

---
+++ Proyecto/Controllers/VentaController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto.Controllers
+{
+    public class VentaController : Controller
+    {
+        //
+        // GET: /Venta/
+
+        public ActionResult Index()
+        {
+            return View("index");
+        }
+
+        public ActionResult indexListado()
+        {
+            return View("indexListado");
+        }
+
+
+        [HttpPost]
+        public JsonResult ObtenerProducto(ProductoWS.Producto objProducto)
+        {
+            var objPrx = new ProductoWS.ProductoServiceClient();
+            ProductoWS.Producto oProducto = objPrx.ObtenerProducto(objProducto.co_producto);
+            return Json(oProducto, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult ObtenerCliente(ClienteWS.Cliente objCliente)
+        {
+            var objPrx = new ClienteWS.ClienteServiceClient();
+
+            ClienteWS.Cliente oCliente = objPrx.ObtenerCliente(objCliente.nu_ruc);
+            return Json(oCliente, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GestionarVenta(VentaWS.Venta objVenta)
+        {
+            var objPrx = new VentaWS.VentaServiceClient();
+
+            VentaWS.Venta oVenta = objPrx.Vender(objVenta.nu_ruc.nu_ruc, objVenta.Detalles);
+            return Json(oVenta, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public JsonResult Listado()
+        {
+            var objPrx = new VentaWS.VentaServiceClient();
+
+            ICollection<VentaWS.Venta> lst = new List<VentaWS.Venta>();
+
+            lst = objPrx.ListarVentas();
+            return Json(lst, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Proyecto/public/js/report/ExportExcel.js
@@ -0,0 +1,53 @@
+﻿function tableToExcel(TableId, Title) {
+
+    var tab_text = "<table border='2px'><tr bgcolor='#87AFC6'>";
+    var textRange; var j = 0;
+    tab = document.getElementById(TableId); // id of table
+
+    for (j = 0 ; j < tab.rows.length ; j++) {
+        tab_text = tab_text + tab.rows[j].innerHTML + "</tr>";
+        tab_text = tab_text + "</tr>";
+    }
+
+    tab_text = tab_text + "</table>";
+    tab_text = tab_text.replace(/<A[^>]*>|<\/A>/g, "");//remove if u want links in your table
+    tab_text = tab_text.replace(/<img[^>]*>/gi, ""); // remove if u want images in your table
+    tab_text = tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // reomves input params
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer
+    {
+        txtArea1.document.open("txt/html", "replace");
+        txtArea1.document.write(tab_text);
+        txtArea1.document.close();
+        txtArea1.focus();
+        sa = txtArea1.document.execCommand("SaveAs", true, Title + ".xls");
+        return (sa);
+
+    }
+    else {              //other browser not tested on IE 11
+        tableToExcel_Chrome(TableId, Title);
+      
+    }
+  
+}
+
+
+
+
+
+var tableToExcel_Chrome = (function () {
+
+    var uri = 'data:application/vnd.ms-excel;base64,'
+      , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta charset="UTF-8" /> <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" /> <meta http-equiv="X-UA-Compatible" content="IE=8; IE=9; IE=10" /> <!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+      , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
+      , format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }) }
+    return function (table, name) {
+        if (!table.nodeType) table = document.getElementById(table)
+        var ctx = { worksheet: name || 'Worksheet', table: table.innerHTML}
+        window.location.href = uri + base64(format(template, ctx))
+    }
+})()
+
