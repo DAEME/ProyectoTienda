4942afbd41945c2772880d3594765021cb8cbc5a
get version <get>
2016-05-31 22:21:09 -0500

    Añadiendo clase Producto para ProductoUnitTest

---
+++ WCFTiendaTest/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFTiendaTest
+{
+    public class Producto
+    {
+        public int co_producto { get; set; }
+        public double nu_precio { get; set; }
+        public string tx_descripcion { get; set; }
+    }
+}
